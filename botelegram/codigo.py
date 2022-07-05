de29f5c8c66c00d98b0e6f15645bce7094259946
ivanduque0 <ivan6a69@gmail.com>
2022-07-04 22:20:21 -0400

    ·la variable de entorno del usuario de la base de datos estaba mal escrita

--- botelegram/codigo.py
+++ botelegram/codigo.py
@@ -317,7 +317,7 @@ while True:
 
         conn = psycopg2.connect(
             database=os.environ.get("DATABASE"), 
-            user=os.environ.get("USER"), 
+            user=os.environ.get("USERDB"), 
             password=os.environ.get("PASSWORD"), 
             host=os.environ.get("HOST"), 
             port=os.environ.get("PORT")
