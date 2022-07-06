5274e83d6714fb60b5d5b84aec8e2885cbbe3ac7
ivanduque0 <ivan6a69@gmail.com>
2022-07-06 04:11:10 -0400

    arreglaron los print que indican cuando se tuvo probelmas con la base de datos o cuando se cierra la base de datos

--- dbmanagerinteracciones/codigo.py
+++ dbmanagerinteracciones/codigo.py
@@ -83,7 +83,7 @@ while True:
                 t1=time.perf_counter()
 
     except (Exception, psycopg2.Error) as error:
-        #print("fallo en hacer las consultas")
+        print("fallo en hacer las consultas")
         if connlocal:
             cursorlocal.close()
             connlocal.close()
@@ -91,10 +91,10 @@ while True:
             cursorheroku.close()
             connheroku.close()
     finally:
+        print("se ha cerrado la conexion a la base de datos")
         if connlocal:
             cursorlocal.close()
             connlocal.close()
         if connheroku:
             cursorheroku.close()
             connheroku.close()
-            #print("se ha cerrado la conexion a la base de datos")
