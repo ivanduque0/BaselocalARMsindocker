80ff85d98c1fc7a2a491cfa2437ef682c06f0e44
ivanduque0 <ivan6a69@gmail.com>
2022-07-28 11:40:09 -0400

    se creo un codigo para manejar todas las caracteristicas de los usuarios de la base de datos, ya no se hace desde distintos codigo debido a que gasta numero de conexiones permitidas a la base de datos en heroku o cualquier otro servicio

---
+++ dbmanagerall/codigo.py
@@ -0,0 +1,247 @@
+import psycopg2
+import os
+import subprocess
+import time
+import pytz
+from datetime import datetime
+from dotenv import load_dotenv
+from pathlib import Path
+
+dotenv_path = Path('/BaselocalARMsindocker/.env.manager')
+load_dotenv(dotenv_path=dotenv_path)
+CONTRATO=os.environ.get("CONTRATO")
+connlocal = None
+connheroku = None
+cursorheroku=None
+cursorlocal=None
+listausuariosheroku=[]
+listausuarioslocal=[]
+total=0
+fechahoy=None
+etapa=0
+
+while True:
+    
+    t1=time.perf_counter()
+    while total<=5:
+        t2=time.perf_counter()
+        total=t2-t1
+    total=0
+    try:
+        
+        #con esto se apunta a la base de datos local
+        connlocal = psycopg2.connect(
+            database=os.environ.get("DATABASE"), 
+            user=os.environ.get("USERDB"), 
+            password=os.environ.get("PASSWORD"), 
+            host=os.environ.get("HOST"), 
+            port=os.environ.get("PORT")
+        )
+        cursorlocal = connlocal.cursor()
+        
+        conn_info = subprocess.run(["heroku", "config:get", "DATABASE_URL", "-a", 'tesis-reconocimiento-facial'], stdout = subprocess.PIPE)
+        connuri = conn_info.stdout.decode('utf-8').strip()
+        connheroku = psycopg2.connect(connuri)
+        cursorheroku = connheroku.cursor()
+
+        while True:
+
+            if etapa==0:
+                tz = pytz.timezone('America/Caracas')
+                caracas_now = datetime.now(tz)
+                fechahoy=str(caracas_now)[:10]
+                cursorlocal.execute('SELECT * FROM web_interacciones where contrato=%s and fecha=%s', (CONTRATO,fechahoy))
+                interacciones_local= cursorlocal.fetchall()
+                cursorheroku.execute('SELECT nombre, fecha, hora, razon, contrato, cedula_id FROM web_interacciones where contrato=%s and fecha=%s', (CONTRATO,fechahoy))
+                interacciones_heroku= cursorheroku.fetchall()
+
+                nro_int_local = len(interacciones_local)
+                nro_int_heroku = len(interacciones_heroku)
+
+                if nro_int_local > nro_int_heroku and total>1:
+
+                    
+
+                    for interaccion in interacciones_local:
+                        try:
+                            interacciones_heroku.index(interaccion)
+                        except ValueError:
+                            nombre=interaccion[0]
+                            fecha=interaccion[1]
+                            hora=interaccion[2]
+                            razon=interaccion[3]
+                            cedula=interaccion[5]
+                            cursorheroku.execute('''INSERT INTO web_interacciones (nombre, fecha, hora, razon, contrato, cedula_id)
+                            VALUES (%s, %s, %s, %s, %s, %s);''', (nombre, fecha, hora, razon, CONTRATO, cedula))
+                            connheroku.commit()
+                    
+                    nombre=None
+                    fecha=None
+                    hora=None
+                    razon=None
+                    cedula=None
+                etapa=1
+
+            if etapa==1:
+
+                cursorlocal.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_local= cursorlocal.fetchall()
+
+                cursorheroku.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_heroku= cursorheroku.fetchall()
+
+
+                for usuario in usuarios_local:
+                    cedula=usuario[0]
+                    try:
+                        listausuarioslocal.index(cedula)
+                    except ValueError:
+                        listausuarioslocal.append(cedula)
+                
+                for usuario in listausuarioslocal:
+                    cursorheroku.execute('SELECT entrada, salida, cedula_id, dia FROM web_horariospermitidos WHERE cedula_id=%s',(usuario,))
+                    diasheroku= cursorheroku.fetchall()
+                    
+                    cursorlocal.execute('SELECT * FROM web_horariospermitidos WHERE cedula_id=%s',(usuario,))
+                    diaslocal= cursorlocal.fetchall()
+
+                    if len(diasheroku) > 0 and len(diasheroku) > len(diaslocal):
+                        for diasherokuiterar in diasheroku:
+                            try:
+                                diaslocal.index(diasherokuiterar)
+                            except ValueError:
+                                entrada=diasherokuiterar[0]
+                                salida=diasherokuiterar[1]
+                                cedula=diasherokuiterar[2]
+                                dia=diasherokuiterar[3]
+                                cursorlocal.execute('''INSERT INTO web_horariospermitidos (entrada, salida, cedula_id, dia)
+                                VALUES (%s, %s, %s, %s);''', (entrada, salida, cedula, dia))
+                                connlocal.commit()
+
+                    if len(diaslocal) > len(diasheroku):
+                        for diaslocaliterar in diaslocal:
+                            try:
+                                diasheroku.index(diaslocaliterar)
+                            except ValueError:
+                                entrada=diaslocaliterar[0]
+                                salida=diaslocaliterar[1]
+                                cedula=diaslocaliterar[2]
+                                dia=diaslocaliterar[3]
+                                cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE entrada=%s AND salida=%s AND cedula_id=%s AND dia=%s',(entrada, salida, cedula, dia))
+                                connlocal.commit()
+                diaslocal=[]
+                diasheroku=[]
+                listausuariosheroku=[]
+                listausuarioslocal=[]
+                etapa=2
+            
+            if etapa==2:
+
+                cursorlocal.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_local= cursorlocal.fetchall()
+
+                cursorheroku.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_heroku= cursorheroku.fetchall()
+                
+                nro_usu_local = len(usuarios_local)
+                nro_usu_heroku = len(usuarios_heroku)
+            
+                if nro_usu_heroku == nro_usu_local:
+                    for usuario in usuarios_heroku:
+                        try:
+                            usuarios_local.index(usuario)
+                        except ValueError:
+                            cedula=usuario[0]
+                            telegram_id=usuario[2]
+                            cursorlocal.execute("UPDATE web_usuarios SET telegram_id=%s WHERE cedula=%s", (telegram_id,cedula))
+                            connlocal.commit()
+                etapa=3
+            
+            if etapa==3:
+
+                cursorlocal.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_local= cursorlocal.fetchall()
+
+                cursorheroku.execute('SELECT * FROM web_usuarios where contrato_id=%s', (CONTRATO,))
+                usuarios_heroku= cursorheroku.fetchall()
+
+                nro_usu_local = len(usuarios_local)
+                nro_usu_heroku = len(usuarios_heroku)
+            
+                #cuando se va a eliminar un usuario
+                if nro_usu_local > nro_usu_heroku:
+
+                    for usuario in usuarios_heroku:
+                        cedula=usuario[0]
+                        try:
+                            listausuariosheroku.index(cedula)
+                        except ValueError:
+                            listausuariosheroku.append(cedula)
+                    
+                    for usuario in usuarios_local:
+                        cedula=usuario[0]
+                        try:
+                            listausuarioslocal.index(cedula)
+                        except ValueError:
+                            listausuarioslocal.append(cedula)
+
+                    for usuario in listausuarioslocal:
+                        try:
+                            listausuariosheroku.index(usuario)
+                        except ValueError:
+                            cursorlocal.execute('DELETE FROM web_usuarios WHERE cedula=%s', (usuario,))
+                            cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE cedula_id=%s', (usuario,))
+                            connlocal.commit()
+                    listausuariosheroku=[]
+                    listausuarioslocal=[]
+
+                # cuando se va a agregar usuarios
+                if nro_usu_heroku > nro_usu_local:
+
+                    for usuario in usuarios_heroku:
+                        cedula=usuario[0]
+                        try:
+                            listausuariosheroku.index(cedula)
+                        except ValueError:
+                            listausuariosheroku.append(cedula)
+                    
+                    for usuario in usuarios_local:
+                        cedula=usuario[0]
+                        try:
+                            listausuarioslocal.index(cedula)
+                        except ValueError:
+                            listausuarioslocal.append(cedula)
+
+                    for usuario in listausuariosheroku:
+                        try:
+                            listausuarioslocal.index(usuario)
+                        except ValueError:
+                            cursorheroku.execute('SELECT * FROM web_usuarios where cedula=%s', (usuario,))
+                            usuario_heroku= cursorheroku.fetchall()
+                            cedula=usuario_heroku[0][0]
+                            nombre=usuario_heroku[0][1]
+                            cursorlocal.execute('''INSERT INTO web_usuarios (cedula, nombre, contrato_id)
+                            VALUES (%s, %s, %s)''', (cedula, nombre, CONTRATO))
+                            connlocal.commit()
+                    listausuariosheroku=[]
+                    listausuarioslocal=[]
+                etapa=0
+
+
+    except (Exception, psycopg2.Error) as error:
+        print("fallo en hacer las consultas")
+        if connlocal:
+            cursorlocal.close()
+            connlocal.close()
+        if connheroku:
+            cursorheroku.close()
+            connheroku.close()
+    finally:
+        print("se ha cerrado la conexion a la base de datos")
+        if connlocal:
+            cursorlocal.close()
+            connlocal.close()
+        if connheroku:
+            cursorheroku.close()
+            connheroku.close()
+            
