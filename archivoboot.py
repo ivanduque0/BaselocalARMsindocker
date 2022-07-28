badd26e158cc61b1aa91faf0a50dafe37a19cf5d
ivanduque0 <ivan6a69@gmail.com>
2022-07-28 11:42:14 -0400

    se me olvido no comentar la linea para arrancar el codigo que maneja el bot

--- archivoboot.py
+++ archivoboot.py
@@ -7,7 +7,7 @@ comandos = [
     # "python3.7 /BaselocalARMsindocker/dbmanagerhorarios/codigo.py &",
     # "python3.7 /BaselocalARMsindocker/dbmanagertelegramid/codigo.py &",
     # "python3.7 /BaselocalARMsindocker/dbmanagerinteracciones/codigo.py &",
-    # "python3.7 /BaselocalARMsindocker/botelegram/codigo.py &"
+    "python3.7 /BaselocalARMsindocker/botelegram/codigo.py &"
 ]
 
 for x in comandos:
