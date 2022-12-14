872b16be8944c309c8fc94f7557995d567193864
ivanduque0 <ivan6a69@gmail.com>
2022-12-14 13:32:59 -0400

    se me habia escapado un archivo al staging area

--- as.py
+++
@@ -1,7 +0,0 @@
-import requests
-ka=[2,3]
-try:
-    ka.index(5)
-    request_json = requests.get(url=f'https://webseguricel.up.railway.app/obtenerusuariosapi/orangepi2g/', auth=('27488274', 'CkretoxDxdxdXd'), timeout=3).json()
-except requests.exceptions.ConnectionError:
-    print("funciona")
