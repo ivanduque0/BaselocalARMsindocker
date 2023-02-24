8769eb596c2d61b89fc112b777a7771bf7234596
ivanduque0 <ivan6a69@gmail.com>
2023-02-24 01:13:20 -0400

    se modifico el formato con lo que se queria

--- formatomensaje.py
+++ formatomensaje.py
@@ -7,7 +7,7 @@ fecha='2023-02-23'
 nombres=['Pedro Perez', 'Alejandro Gonzales', 'Ernesto Villa']
 
 for nombre in nombres:
-    mensaje=f'INVITACION PARA {contrato}\n\nFecha:{fecha}\nNombre: {nombre}\nCodigo: 123456\n\nLinks de descarga: \n\nAndroid={link}\n\niOs={link}'
+    mensaje=f'INVITACION RES. {contrato}\n\nFecha:{fecha}\nNombre: {nombre}\nCodigo: 123456\n\nSi desea abrir con su telefono por proximidad via BLuetooth, descargue la aplicacion: \n\nAndroid: {link}\n\niOs: {link}'
     request_mensaje = requests.get(url=f'http://api.callmebot.com/whatsapp.php?phone=584122810793&text=!sendto+{numero_enviar}+{mensaje}&apikey=5525175', timeout=3)
     # request_mensaje = requests.get(url=f'http://api.callmebot.com/whatsapp.php?phone=584122810793&text=!sendto+{numero_enviar}+{mensaje}&apikey=5525175', timeout=3)
     print(request_mensaje)
