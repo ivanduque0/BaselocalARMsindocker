9795ee7eb5fe6f78bab98b173dc952aad200d836
ivanduque0 <ivan6a69@gmail.com>
2023-03-11 18:27:05 -0400

    se agrego un archivo boot para que se ejecute en placas que tengan python3

---
+++ archivoboot2.py
@@ -0,0 +1,20 @@
+import os
+
+comandos = [
+    "python3 /BaselocalARMsindocker/dbmaker/codigo.py &",
+    "python3 /BaselocalARMsindocker/dbmanagerall/codigo.py &",
+    "python3 /BaselocalARMsindocker/dispositivosweb/codigo.py &",
+    # "python3.7 /BaselocalARMsindocker/dbmanagerusuarios/codigo.py &",
+    # "python3.7 /BaselocalARMsindocker/dbmanagerhorarios/codigo.py &",
+    # "python3.7 /BaselocalARMsindocker/dbmanagertelegramid/codigo.py &",
+    # "python3.7 /BaselocalARMsindocker/dbmanagerinteracciones/codigo.py &",
+    #"python3.7 /BaselocalARMsindocker/botelegram/codigo.py &",
+    "python3 /BaselocalARMsindocker/servidorlocalwifi/codigo.py &",
+    "python3 /BaselocalARMsindocker/aperturas/codigo.py &",
+    #"python3.7 /BaselocalARMsindocker/pingdispositivos/codigo.py &",
+]
+
+for x in comandos:
+    os.system(x)
+
+print('hecho!')
