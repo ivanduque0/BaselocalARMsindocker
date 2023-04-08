21b9f0c60544bb37e27da5f4565378257883a0b4
ivanduque0 <ivan6a69@gmail.com>
2023-04-08 07:13:01 -0400

    se cambio de posicion el borrado del control de visitantes

--- dbmanagerall/codigo.py
+++ dbmanagerall/codigo.py
@@ -992,8 +992,8 @@ while True:
                             try:
                                 request_json = requests.delete(url=f'{URL_API}editarhorariosvisitantesapi/{horario[0]}/', auth=('BaseLocal_access', 'S3gur1c3l_local@'), timeout=10)
                                 if request_json.status_code == 200 or request_json.status_code == 500:
-                                    cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                     cursorlocal.execute('DELETE FROM control_horarios_visitantes WHERE horario_id=%s', (horario[0],))
+                                    cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                     connlocal.commit() 
                             except Exception as e:
                                 print(f"{e} - fallo total eliminando horario:{horario[0]}") 
@@ -1001,8 +1001,8 @@ while True:
                             try:
                                 request_json = requests.delete(url=f'{URL_API}editarhorariosvisitantesapi/{horario[0]}/', auth=('BaseLocal_access', 'S3gur1c3l_local@'), timeout=10)
                                 if request_json.status_code == 200 or request_json.status_code == 500:
-                                    cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                     cursorlocal.execute('DELETE FROM control_horarios_visitantes WHERE horario_id=%s', (horario[0],))
+                                    cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                     cursorlocal.execute('DELETE FROM web_usuarios WHERE id=%s', (horario[1],))
                                     connlocal.commit() 
                             except Exception as e:
@@ -1016,8 +1016,8 @@ while True:
                                 try:
                                     request_json = requests.delete(url=f'{URL_API}editarhorariosvisitantesapi/{horario[0]}/', auth=('BaseLocal_access', 'S3gur1c3l_local@'), timeout=10)
                                     if request_json.status_code == 200 or request_json.status_code == 500:
-                                        cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                         cursorlocal.execute('DELETE FROM control_horarios_visitantes WHERE horario_id=%s', (horario[0],))
+                                        cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                         connlocal.commit() 
                                 except Exception as e:
                                     print(f"{e} - fallo total eliminando horario:{horario[0]}") 
@@ -1030,8 +1030,8 @@ while True:
                                 try:
                                     request_json = requests.delete(url=f'{URL_API}editarhorariosvisitantesapi/{horario[0]}/', auth=('BaseLocal_access', 'S3gur1c3l_local@'), timeout=10)
                                     if request_json.status_code == 200 or request_json.status_code == 500:
-                                        cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                         cursorlocal.execute('DELETE FROM control_horarios_visitantes WHERE horario_id=%s', (horario[0],))
+                                        cursorlocal.execute('DELETE FROM web_horariospermitidos WHERE id=%s', (horario[0],))
                                         cursorlocal.execute('DELETE FROM web_usuarios WHERE id=%s', (horario[1],))
                                         connlocal.commit() 
                                 except Exception as e:
